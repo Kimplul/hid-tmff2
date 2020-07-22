9826e1b5cef24cd20ad728393822926ff4cf10e7
Kimplul <kimi.h.kuparinen@gmail.com>
2020-07-22 20:36:37 +0300

    Revert "jesus christ"
    This reverts commit 6b368278f9380a7ad5f891f7cb43cfaa09609575.

---
+++ .hid-tminit.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/kimi/Documents/Projects/hid-tmff2/hid-tminit.mod := { echo  /home/kimi/Documents/Projects/hid-tmff2/hid-tminit.o; echo; } > /home/kimi/Documents/Projects/hid-tmff2/hid-tminit.mod
---
+++ .hid-tmt300rs.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/kimi/Documents/Projects/hid-tmff2/hid-tmt300rs.mod := { echo  /home/kimi/Documents/Projects/hid-tmff2/hid-tmt300rs.o; echo; } > /home/kimi/Documents/Projects/hid-tmff2/hid-tmt300rs.mod
